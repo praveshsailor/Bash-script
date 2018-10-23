895e4203b8fbfb6b137e641ad2b7eb3ce4294c78
praveshsailor <praveshsailor@gmail.com>
2018-10-23 16:53:37 +0530

    bash script for separate windows,linux and stoped servers list

---
+++ SeperateSerevrs.sh
@@ -0,0 +1,14 @@
+#!bin/bash
+for line in $(cat /opt/ipm-test.txt) ; do
+echo $line
+sleep 1
+ttl=$(ping -c 1 $line | grep ttl | awk '{ print $6 }')
+echo $ttl
+if [ $ttl = "ttl=63" -o $ttl = "ttl=64" ]  ; then
+echo $line >> /opt/linux.txt
+elif [ $ttl = "ttl=128" -o  $ttl = "ttl=127" ]; then
+echo $line >> /opt/windows.txt
+else
+echo $line >> /opt/shutdown.txt
+fi
+done
