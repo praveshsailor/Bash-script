a90be5f373db146ed1c126a7e3aec53f2eb8246a
praveshsailor <praveshsailor@gmail.com>
2018-10-25 18:57:21 +0530

    Resolved unexpected operator

--- SeperateSerevrs.sh
+++ SeperateSerevrs.sh
@@ -3,10 +3,10 @@ for line in $(cat /opt/ipm-test.txt) ; do
 echo $line
 sleep 1
 ttl=$(ping -c 1 $line | grep ttl | awk '{ print $6 }')
-echo $ttl
-if [ $ttl = "ttl=63" -o $ttl = "ttl=64" ]  ; then
+#echo $ttl
+if [ "$ttl" == "ttl=63" -o "$ttl" == "ttl=64" ]  ; then
 echo $line >> /opt/linux.txt
-elif [ $ttl = "ttl=128" -o  $ttl = "ttl=127" ]; then
+elif [ "$ttl" == "ttl=128" -o  "$ttl" == "ttl=127" ]; then
 echo $line >> /opt/windows.txt
 else
 echo $line >> /opt/shutdown.txt
